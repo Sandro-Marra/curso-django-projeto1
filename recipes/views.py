fe2dfe85540ddec636f13b1bd18c94b2464e9bdc
Sandro Marra <sandro.marra@luizalabs.com>
2023-01-04 14:21:48 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('HOME 2')
-
-
-def contato(request):
-    return HttpResponse('contato')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
+    return render(request, 'recipes/home.html')

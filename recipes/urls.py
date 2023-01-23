91f4322cdc3cb184f508020c01d41b31ceb3d44a
Sandro Marra <sandro.marra@luizalabs.com>
2023-01-23 17:14:34 -0300

    Criaçã ode nome único nas urls e app_name

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+
 from . import views
 
 
+app_name = 'recipes'
+
+
 urlpatterns = [
-    path('', views.home),  # home
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name="home"),  # home
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
